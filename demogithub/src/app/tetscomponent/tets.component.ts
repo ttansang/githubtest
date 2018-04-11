4fa87a4a47dc5269352189ad67dd9fd2c1651c0e
TMA\ttansang <ttansang@tma.com.vn>
2018-04-11 16:33:32 +0700

    import test component into app folder

---
+++ demogithub/src/app/tetscomponent/tets.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './tets.component.html',
+  styleUrls: ['./tets.component.css']
+})
+export class TetsComponent {
+  title = 'app';
+}
+
